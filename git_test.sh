2a8532a29aed3e562908f62d3873279f49c23950
davron1989 <davron.n@yahoo.com>
2021-03-19 21:16:18 +0000

    updated setenv

--- git_test.sh
+++ git_test.sh
@@ -4,13 +4,13 @@ git remote update &> /dev/null
 git status -uno | grep -i "Your branch is behind 'origin/main'"  &> /dev/null
 
 if [ $? -eq 0 ]; then
-  read -p "there has been changes in master barnch do you want to git pull\n:" yes
+  read -p "there has been changes in master barnch do you want to git pull: " yes
   if [[ $yes == yes ]] || [[ $yes == y ]] || [[ $yes == Y ]]; then
     git pull
     echo "Your local branch is up to date with origin/master now"
   else
-    echo "Skipping git pull, your local branch is not up to date with remote master"
+    echo "Skipping git pull, your local branch is not up to date with master branch"
   fi
-elif [ $? -eq 0 ]; then
-  echo "You branch is up to date"
+elif [ $? -eq 1 ]; then
+  echo "You branch is up to date. No need for 'git pull'"
 fi
